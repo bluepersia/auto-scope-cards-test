db1fbec5624aeca4b2e1e3318bc9d5bce648d33d
Marco Zimmermann <fuzzyslash@hotmail.com>
2025-07-02 23:19:34 -0600

    New build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import { execSync } from 'child_process';
 
-export default defineConfig({
+export default defineConfig({plugins: [
+    {
+      name: 'pre-build-fluid-scale',
+      buildStart() {
+        execSync('npx fluid-build', { stdio: 'inherit' });
+      },
+    },
+  ],
   root: 'vite',
   publicDir: '../public',
   build: {
